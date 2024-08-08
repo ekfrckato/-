aa8f214845289789658675bb86d17e611a5eeec9
ekfrckato <94157543+ekfrckato@users.noreply.github.com>
2024-08-08 13:48:47 +0900

    Update absoulute_slave.py

--- controller/absoulute_slave.py
+++ controller/absoulute_slave.py
@@ -13,6 +13,8 @@ class EncoderControll(Node):
         # 전역 변수로 present_voltage0, present_voltage1 선언
         self.present_voltage0 = 0.0
         self.present_voltage1 = 0.0
+        self.zero_area_linear = 6
+        self.zero_area_angular = 9
         
         # publisher 설정
         self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
@@ -40,7 +42,6 @@ class EncoderControll(Node):
 
         print("complete ", self.init_position0, self.init_position1)
 
-
         # 타이머 설정 (0.1초마다 pub_encoder_val 호출)
         self.timer = self.create_timer(0.01, self.pub_encoder_val)
         
@@ -62,34 +63,40 @@ class EncoderControll(Node):
         acctual_angular = acctual_position0 - self.init_position0
         acctual_linear = acctual_position1 - self.init_position1
 
-        print(f"present 0 Position: {acctual_angular}")
-        print(f"present 1 Position: {acctual_linear}")
+        # print(f"present 0 Position: {acctual_angular}")
+        # print(f"present 1 Position: {acctual_linear}")
 
-        zero_area_linear = 6
-        zero_area_angular = 9
-        if -zero_area_angular < acctual_angular < zero_area_angular:
+        #define safe controll area
+        if -self.zero_area_angular < acctual_angular < self.zero_area_angular:
             angular= 0
         else:
-            if zero_area_angular < acctual_angular :
-                angular = acctual_angular - zero_area_angular
-            elif acctual_angular < -zero_area_angular:
-                angular = acctual_angular + zero_area_angular
+            if self.zero_area_angular < acctual_angular :
+                angular = acctual_angular - self.zero_area_angular
+            elif acctual_angular < -self.zero_area_angular:
+                angular = acctual_angular + self.zero_area_angular
 
-        if -zero_area_linear < acctual_linear < zero_area_linear:
+        if -self.zero_area_linear < acctual_linear < self.zero_area_linear:
             linear = 0
         else:
-            if zero_area_linear < acctual_linear :
-                linear = acctual_linear - zero_area_linear
-            elif acctual_linear < -zero_area_linear:
-                linear = acctual_linear + zero_area_linear
+            if self.zero_area_linear < acctual_linear :
+                linear = acctual_linear - self.zero_area_linear
+            elif acctual_linear < -self.zero_area_linear:
+                linear = acctual_linear + self.zero_area_linear
 
         linear_max = 0.5
         angular_max = 1.5
         linear = (linear/23)*0.5 
         angular = (angular/50)*1.5
 
-        pub_angler_vel = angular * linear / linear_max
+
+        if linear == 0:
+            pub_angler_vel = angular
+        else :
+            pub_angler_vel = angular * linear / linear_max
         pub_linear_vel = linear
+
+        # if linear == 0 and angular > 0.25 :
+
 
         print(f"linear: {linear} + cmd : {Vector3(x=pub_linear_vel, y=0.0, z=0.0)}")
         print(f"angular: {angular} + cmd : {Vector3(x=0.0, y=0.0, z=pub_angler_vel)}")
