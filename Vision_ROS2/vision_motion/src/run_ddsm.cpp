70d0e868be5a18d3124b9b442f8d7b953471e4fd
ekfrckato <94157543+ekfrckato@users.noreply.github.com>
2024-08-12 13:44:39 +0900

    Add files via upload

---
+++ Vision_ROS2/vision_motion/src/run_ddsm.cpp
@@ -0,0 +1,119 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <cmath>
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "vision_motion/motor_command.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+#define PI 3.1415926535897932
+
+class DDSM_Twist : public rclcpp::Node {
+public:
+    DDSM_Twist() : Node("DDSM115"), port1(0), port2(2), port3(4), port4(6){
+        declare_parameter("WO1", 0.54);
+        declare_parameter("WO2", 0.04);
+        declare_parameter("WO3", 0.29);
+        declare_parameter("AW", 0.40);
+        declare_parameter("WD", 0.1);
+        get_parameter("WO1", wheelOffset1_);
+        get_parameter("WO2", wheelOffset2_);
+        get_parameter("WO3", wheelOffset3_);
+        get_parameter("AW", axleWidth_);
+        get_parameter("WD", wheelDiameter_);
+
+        twist_sub_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 1, std::bind(&DDSM_Twist::twist_cb, this, _1));
+        timer_ = this->create_wall_timer(50ms, std::bind(&DDSM_Twist::timer_cb, this));
+    }
+private:
+    void timer_cb() {
+        port1.SET_VELOCITY(1, motor_rpm_[0]);
+        port2.SET_VELOCITY(1, motor_rpm_[1]);
+        if (timer == 0) {
+            port3.SET_VELOCITY(1, motor_rpm_[2]);
+            port4.SET_VELOCITY(1, motor_rpm_[3]);
+            timer = 1;
+        }
+        else {
+            port3.SET_VELOCITY(2, motor_rpm_[4]);
+            port4.SET_VELOCITY(2, motor_rpm_[5]);
+            timer = 0;
+        }
+    }
+
+    void twist_cb(const geometry_msgs::msg::Twist msg) {
+        double v = msg.linear.x;
+        double omega = msg.angular.z;
+        auto radius = std::abs(v / omega);
+        int ZeroRPM = 3
+
+        wheel_omega[0] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset1_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[1] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset1_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[2] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset2_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[3] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset2_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[4] = v * sqrt(pow((radius - axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
+        wheel_omega[5] = v * sqrt(pow((radius + axleWidth_ / 2), 2) + pow((wheelOffset3_ / 2), 2)) / radius / wheelDiameter_ * 2;
+
+        if (std::abs(omega) > 0.25 && radius == 0.) {
+            if (omega > 0.01) {
+                motor_rpm_[0] = ZeroRPM
+                motor_rpm_[1] = ZeroRPM
+                motor_rpm_[2] = (ZeroRPM * wheelOffset2_ / wheelOffset1_)
+                motor_rpm_[3] = (ZeroRPM * wheelOffset2_ / wheelOffset1_)
+                motor_rpm_[4] = ZeroRPM
+                motor_rpm_[5] = ZeroRPM
+                }
+            else if (omega < -0.01) {
+                motor_rpm_[0] = -ZeroRPM
+                motor_rpm_[1] = -ZeroRPM
+                motor_rpm_[2] = -(ZeroRPM * wheelOffset2_ / wheelOffset1_)
+                motor_rpm_[3] = -(ZeroRPM * wheelOffset2_ / wheelOffset1_)
+                motor_rpm_[4] = -ZeroRPM
+                motor_rpm_[5] = -ZeroRPM
+                    }
+            }      
+        else if (omega < -0.01 && radius > 0.01) {
+            motor_rpm_[0] = wheel_omega[1] * 30 / PI;
+            motor_rpm_[1] = -1 * wheel_omega[0] * 30 / PI;
+            motor_rpm_[2] = wheel_omega[3] * 30 / PI;
+            motor_rpm_[3] = -1 * wheel_omega[2] * 30 / PI;
+            motor_rpm_[4] = wheel_omega[5] * 30 / PI;
+            motor_rpm_[5] = -1 * wheel_omega[4] * 30 / PI;
+        }
+        else if(omega > 0.01 && radius > 0.01) {
+            motor_rpm_[0] = wheel_omega[0] * 30 / PI;
+            motor_rpm_[1] = -1 * wheel_omega[1] * 30 / PI;
+            motor_rpm_[2] = wheel_omega[2] * 30 / PI;
+            motor_rpm_[3] = -1 * wheel_omega[3] * 30 / PI;
+            motor_rpm_[4] = wheel_omega[4] * 30 / PI;
+            motor_rpm_[5] = -1 * wheel_omega[5] * 30 / PI;
+        }
+        else {
+            motor_rpm_[0] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[1] = -1 * (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[2] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[3] = -1 * (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[4] = (60 * v) / (wheelDiameter_ * PI);
+            motor_rpm_[5] = -1 * (60 * v) / (wheelDiameter_ * PI);
+        }
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr twist_sub_;
+    double wheel_omega[6];
+    double motor_rpm_[6] = {0.0, }; // LF, RF, LM, RM, LR, RR
+    MOTOR_COMMAND port1, port2, port3, port4;
+    double wheelOffset1_, wheelOffset2_, wheelOffset3_,axleWidth_, wheelDiameter_;
+    int timer = 0; // 0: middle wheel, 1: rear wheel
+};
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DDSM_Twist>());
+    rclcpp::shutdown();
+    return 0;
+}
---
+++ Vision_ROS2/vision_motion/src/run_opencm.cpp
@@ -0,0 +1,72 @@
+#include "vision_motion/run_opencm.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+DynamixelController::DynamixelController() : Node("dynamixel_controller") {
+    declare_parameter("WO1", 0.54);
+    declare_parameter("WO2", 0.04);
+    declare_parameter("WO3", 0.29);
+    declare_parameter("AW", 0.40);
+    get_parameter("WO1", wheelOffset1_);
+    get_parameter("WO2", wheelOffset2_);
+    get_parameter("WO3", wheelOffset3_);
+    get_parameter("AW", axleWidth_);
+    
+    sub_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&DynamixelController::twist_cb ,this, _1));
+    timer_ = this->create_wall_timer(10ms, std::bind(&DynamixelController::timer_cb, this));
+    std::fill(motPos_, motPos_ + 6, 3073);
+}
+void DynamixelController::twist_cb(const geometry_msgs::msg::Twist msg) {
+    double radius = std::abs(msg.linear.x / msg.angular.z);
+    
+    if (msg.angular.z < -0.01 && radius > 0.01) {
+        motPos_[0] = 3073 + (std::atan2(wheelOffset1_, radius + (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[1] = 3073 + (std::atan2(wheelOffset1_, radius - (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[2] = 3073 + (std::atan2(wheelOffset2_, radius + (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[3] = 3073 + (std::atan2(wheelOffset2_, radius - (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[4] = 3073 - (std::atan2(wheelOffset3_, radius + (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[5] = 3073 - (std::atan2(wheelOffset3_, radius - (axleWidth_ / 2)) * 3073 / PI);
+    }
+    else if (msg.angular.z > 0.01 && radius > 0.01) {
+        motPos_[0] = 3073 - (std::atan2(wheelOffset1_, radius - (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[1] = 3073 - (std::atan2(wheelOffset1_, radius + (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[2] = 3073 - (std::atan2(wheelOffset2_, radius - (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[3] = 3073 - (std::atan2(wheelOffset2_, radius + (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[4] = 3073 + (std::atan2(wheelOffset3_, radius - (axleWidth_ / 2)) * 3073 / PI);
+        motPos_[5] = 3073 + (std::atan2(wheelOffset3_, radius + (axleWidth_ / 2)) * 3073 / PI);
+    }
+    else if (std::abs(msg.angular.z) > 0.03 && radius < 0.01) {
+        std::cout << "for zero_radious turn  " << std::endl;
+        double calculation = (std::atan2(wheelOffset1_ + wheelOffset3_, axleWidth_) * 3073 / M_PI);
+        std::cout << "Calculation result: " << calculation << std::endl;
+        motPos_[0] = 3073 + (std::atan2(wheelOffset1_ + wheelOffset3_, axleWidth_)* 3073 / PI);
+        motPos_[1] = 3073 - (std::atan2(wheelOffset1_ + wheelOffset3_, axleWidth_)* 3073 / PI);
+        motPos_[2] = 3073 + (std::atan2(wheelOffset2_ , axleWidth_)* 3073 / PI);
+        motPos_[3] = 3073 - (std::atan2(wheelOffset2_ , axleWidth_)* 3073 / PI);
+        motPos_[4] = 3073 - (std::atan2(wheelOffset1_ + wheelOffset3_, axleWidth_)* 3073 / PI);
+        motPos_[5] = 3073 + (std::atan2(wheelOffset1_ + wheelOffset3_, axleWidth_)* 3073 / PI);
+    }   
+    else {
+        motPos_[0] = 3073;
+        motPos_[1] = 3073;
+        motPos_[2] = 3073;
+        motPos_[3] = 3073;
+        motPos_[4] = 3073;
+        motPos_[5] = 3073;
+    }
+}
+void DynamixelController::timer_cb() {
+    writePosition(motPos_);
+}
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<DynamixelController>());
+    rclcpp::shutdown();
+    return 0;
+}
