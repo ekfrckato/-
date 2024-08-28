fd16bd46b158a1e8d990352133006fd80c519304
ekfrckato <94157543+ekfrckato@users.noreply.github.com>
2024-08-28 20:12:06 +0900

    Update teleop_keyboard.py

--- Vision_ROS2/teleop_keyboard/teleop_keyboard/teleop_keyboard.py
+++ Vision_ROS2/teleop_keyboard/teleop_keyboard/teleop_keyboard.py
@@ -2,9 +2,13 @@ import os
 import select
 import sys
 import rclpy
-
+from std_msgs.msg import String
+import sys, tty, termios
+import threading
+from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from rclpy.qos import QoSProfile
+from rclpy.executors import MultiThreadedExecutor
 
 if os.name == 'nt':
     import msvcrt
@@ -16,7 +20,7 @@ MAX_LIN_VEL = 1.20
 MAX_ANG_VEL = 1.00
 
 LIN_VEL_STEP_SIZE = 0.10
-ANG_VEL_STEP_SIZE = 0.05
+ANG_VEL_STEP_SIZE = 0.10
 
 msg = """
 ---------------------------
@@ -47,30 +51,16 @@ def get_key(settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
-
 def print_vels(target_linear_velocity, target_angular_velocity):
     print('currently:\tlinear velocity {0}\t angular velocity {1} '.format(
         target_linear_velocity,
         target_angular_velocity))
-
-def make_simple_profile(output, input, slop):
-    if input > output:
-        output = min(input, output + slop)
-    elif input < output:
-        output = max(input, output - slop)
-    else:
-        output = input
-
-    return output
 
 def constrain(input_vel, low_bound, high_bound):
     if input_vel < low_bound:
         input_vel = low_bound
     elif input_vel > high_bound:
         input_vel = high_bound
-    else:
-        input_vel = input_vel
-
     return input_vel
 
 
@@ -90,47 +80,58 @@ def main():
     rclpy.init()
 
     qos = QoSProfile(depth=10)
-    node = rclpy.create_node('teleop_keyboard')
-    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    node_wheel = rclpy.create_node('teleop_keyboard')
+    pub_wheel = node_wheel.create_publisher(Twist, 'cmd_vel', qos)
+
+    pole_msg = String()
+    node_pole = rclpy.create_node('control_pole')
+    pub_pole = node_pole.create_publisher(String, '/stairs_check', qos)
 
     status = 0
     target_linear_velocity = 0.0
     target_angular_velocity = 0.0
-    control_linear_velocity = 0.0
-    control_angular_velocity = 0.0
+
+    executor = MultiThreadedExecutor()
+    executor.add_node(node_wheel)
+    executor.add_node(node_pole)
+    
+    thread = threading.Thread(target=executor.spin)
+    thread.start()
 
     try:
         print(msg)
-        while(1):
+        while True:
             key = get_key(settings)
-            if key == 'w':
+            if key == 'w' or key == 'W':
                 target_linear_velocity =\
                     check_linear_limit_velocity(target_linear_velocity + LIN_VEL_STEP_SIZE)
-                status = status + 1
+                status += 1
                 print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'x':
+            elif key == 'x' or key == 'X':
                 target_linear_velocity =\
                     check_linear_limit_velocity(target_linear_velocity - LIN_VEL_STEP_SIZE)
-                status = status + 1
+                status += 1
                 print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'a':
+            elif key == 'a' or key == 'A':
                 target_angular_velocity =\
                     check_angular_limit_velocity(target_angular_velocity + ANG_VEL_STEP_SIZE)
-                status = status + 1
+                status += 1
                 print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == 'd':
+            elif key == 'd' or key == 'D':
                 target_angular_velocity =\
                     check_angular_limit_velocity(target_angular_velocity - ANG_VEL_STEP_SIZE)
-                status = status + 1
+                status += 1
                 print_vels(target_linear_velocity, target_angular_velocity)
-            elif key == ' ' or key == 's':
+            elif key == ' ' or key == 's' or key == 'S':
                 target_linear_velocity = 0.0
-                control_linear_velocity = 0.0
                 target_angular_velocity = 0.0
-                control_angular_velocity = 0.0
                 print_vels(target_linear_velocity, target_angular_velocity)
+            elif key == 'e' or key == 'E':
+                pole_msg.data = 'aaa'
+                pub_pole.publish(pole_msg)
+                print("Successfully sent the msg for pole")
             else:
-                if (key == '\x03'):
+                if key == '\x03':
                     break
 
             if status == 20:
@@ -138,26 +139,14 @@ def main():
                 status = 0
 
             twist = Twist()
-
-            control_linear_velocity = make_simple_profile(
-                control_linear_velocity,
-                target_linear_velocity,
-                (LIN_VEL_STEP_SIZE / 2.0))
-
-            twist.linear.x = control_linear_velocity
+            twist.linear.x = target_linear_velocity
             twist.linear.y = 0.0
             twist.linear.z = 0.0
-
-            control_angular_velocity = make_simple_profile(
-                control_angular_velocity,
-                target_angular_velocity,
-                (ANG_VEL_STEP_SIZE / 2.0))
-
             twist.angular.x = 0.0
             twist.angular.y = 0.0
-            twist.angular.z = control_angular_velocity
+            twist.angular.z = target_angular_velocity
 
-            pub.publish(twist)
+            pub_wheel.publish(twist)
 
     except Exception as e:
         print(e)
@@ -167,12 +156,15 @@ def main():
         twist.linear.x = 0.0
         twist.linear.y = 0.0
         twist.linear.z = 0.0
-
         twist.angular.x = 0.0
         twist.angular.y = 0.0
         twist.angular.z = 0.0
 
-        pub.publish(twist)
+        pub_wheel.publish(twist)
+
+        node_wheel.destroy_node()
+        node_pole.destroy_node()
+        rclpy.shutdown()
 
         if os.name != 'nt':
             termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
