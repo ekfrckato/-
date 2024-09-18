094a95d240f90e20c6409014bfea4e67fd5a5442
ekfrckato <94157543+ekfrckato@users.noreply.github.com>
2024-09-18 20:45:30 +0900

    Update pub_diff_stairs_depth_imp.py

--- stairs_depth/stairs_depth/pub_diff_stairs_depth_imp.py
+++ stairs_depth/stairs_depth/pub_diff_stairs_depth_imp.py
@@ -15,12 +15,17 @@ class DepthArraySubscriber(Node):
         self.publisher_diff_array = self.create_publisher(Float32MultiArray, '/diff_array_msg', 10)
         self.publisher_depth_img = self.create_publisher(Float32MultiArray, '/depth_imp_img', 10)
         self.publisher_stairs_check = self.create_publisher(String , '/stairs_check', 10)
- 
+        self.get_data = []
+        
+    def capture_data(self, gradients):
+        self.get_data.add(gradients) 
+        
+    
     def opencv_graph(self, msg):
         depth_array = msg.data
         
-        height = 480
-        width = 640
+        height = 240
+        width = 320
         img = np.zeros((height, width, 3), dtype=np.uint8)
         
         if len(depth_array) == 0:
@@ -31,6 +36,7 @@ class DepthArraySubscriber(Node):
         x = np.arange(len(depth_array))
         y = np.array(depth_array)
 
+        # 0값에 가까운 잡음 처리
         noise_value_num = [i for i in range(len(y)) if np.isclose(y[i], 0.0, atol=1e-6)] 
         for idx in noise_value_num:
             if idx == 0:
@@ -47,61 +53,66 @@ class DepthArraySubscriber(Node):
         y_scaled = height - y_scaled  # Y축 반전 적용
 
         # 기울기 계산
-        #len(gradients) = 360
-        #np.diff() : 
-        gradients = np.diff(y_scaled)  # y_scaled에 대한 기울기 계산
+        gradients = np.diff(y_scaled)
         print(gradients)
 
-        #publish
-        gradients2list=gradients.tolist()
+        # 퍼블리시 관련 부분은 그대로 유지
+        gradients2list = gradients.tolist()
         gradient_msg = Float32MultiArray()
         gradient_msg.data = gradients2list
         self.publisher_diff_array.publish(gradient_msg)
 
-        #hing hagisiro
-        i = 0
+        # 계단 여부 체크 후 퍼블리시
+        # 계단 여부 체크 후 퍼블리시
+        wall_diff = -3.6
+        wall_height = 15
+        floor_diff = 1
+        floor_width = 10
         index_wall_N = 0
         index_floor_N = 0
-        wall_diff = 10
-        wall_height = 10
-        floor_diff = 22
-        floor_width = 30
         stair_being_there = False
+
         for i in range(len(gradients)):
-            if i is None : gradients[i:] = 0
-            for j, x in enumerate(gradients[i:]):
-                # j = correct the num 
-                if x > wall_diff:
-                    index_wall_N =+ 1
-                    break
-
-            if j > wall_height:
-                for k, y in enumerate(gradients[j+i:]):
-                    if (y > floor_diff) & (k < floor_width):
-                        index_floor_N =+ 1
-            if (index_wall_N >2) & (index_floor_N > 1):
+            # 15픽셀 이상이고 -3.6 이하의 기울기 조건
+            if gradients[i] < wall_diff:
+                index_wall_N += 1
+            else:
+                continue
+            
+            # 벽의 높이가 15픽셀 이상인 경우
+            if index_wall_N >= wall_height:
+                # 벽 이후 10픽셀 이상이고 1 이상의 기울기가 연속적으로 나오는지 확인
+                for j in range(i + 1, len(gradients)):
+                    if gradients[j] > floor_diff and (j - i) > floor_width:
+                        index_floor_N += 1
+                        break
+            
+            # 벽이 15픽셀 이상이고, 10픽셀 이상 1 이상의 기울기가 연속적으로 나오는 경우
+            if index_wall_N >= wall_height and index_floor_N >= 1:
                 stair_being_there = True
+                break
 
-        if stair_being_there is True :
-            print('there is stair')
+        if stair_being_there:
+            print('계단이 있음')
             stairs_check = String()
-            stairs_check.data = 'there is stairs'
+            stairs_check.data = 'over'
             self.publisher_stairs_check.publish(stairs_check)
 
-        #publish
-        if np.size(y) == 0 :
+        # xnew와 y_scaled 그래프 그리기 부분
+        if np.size(y) == 0:
             xnew = np.linspace(0, 1, 1)
-        else :
+        else:
             xnew = np.linspace(0, np.size(y) - 1, num=np.size(y))
-        xnew.tolist()
-        depth_imp_img = Float32MultiArray([xnew, y_scaled])
-        depth_imp_img.data = depth_imp_img
-        self.publisher_depth_img.publish(depth_imp_img)
+
         prev_point = None
-        # xnew와 y를 사용해 그래프를 그릴 수 있도록 데이터 스케일 조정
-        
+        max_xnew = np.max(xnew)
+        if np.isnan(max_xnew) or max_xnew == 0:
+            print("Invalid max value for xnew")
+            return
+
+        # 퍼블리시 제거, 그래프만 그리도록 변경
         for i, (x_val, y_val) in enumerate(zip(xnew, y_scaled)):
-            x_curr = int(width - ((x_val / np.max(xnew)) * width))
+            x_curr = int(width - ((x_val / max_xnew) * width))
             y_curr = int(y_val)
             curr_point = (x_curr, y_curr)
             
